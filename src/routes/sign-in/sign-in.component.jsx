ba63e7605abcbe4fe1a508db426972f0f898f749
CHZelaya <chzelaya1201@gmail.com>
2022-09-21 18:30:40 -0600

    #1 Finished part one of the sign up forms and authentication

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils.js";
+import { React } from 'react';
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component.jsx';
+
+
+import {
+    signInWithGooglePopup,
+    createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils.js";
+
 
 const SignIn = () => {
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
-        createUserDocumentFromAuth(user);
+        const userDocRef = await createUserDocumentFromAuth(user);
     }
+
+
+
+
+
     return (
         <div>
             <h1>Sign In Page</h1>
-            <button onClick={logGoogleUser}>
-                Sign in with Google PopUp
-            </button>
+            <button onClick={logGoogleUser}> Sign in with Google PopUp </button>
+            <SignUpForm />
         </div>
 
     )
