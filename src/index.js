fc5b0555506dc908de9a8331b5ae5653dffb0735
CHZelaya <chzelaya1201@gmail.com>
2022-09-29 22:41:18 -0600

    #30 Added toggle functionality

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './index.scss';
 import { UserProvider } from '../src/contexts/user.context.jsx';
 import reportWebVitals from './reportWebVitals';
 import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,7 +14,9 @@ root.render(
     <BrowserRouter>
       <UserProvider>
         <ProductsProvider>
-          <App />
+          <CartProvider>
+            <App />
+          </CartProvider>
         </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
